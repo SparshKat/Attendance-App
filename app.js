b338fccb8ff7b334cd297bd63ba5998b130e4ed7
shivanggarg1998 <shivanggarg1998@gmail.com>
2020-03-28 23:10:17 +0530

    mongoose connect

--- app.js
+++ app.js
@@ -1,49 +1,50 @@
-var     express         = require('express') ,
-        bodyParser      = require('body-parser') ,
-        mongoose        = require('mongoose') ,
-        methodOverride  = require("method-override"),
-        MongoClient     = require('mongodb').MongoClient,
-        port            = process.env.PORT || 8080,
-        router          = express.Router(),
-        app             = express();
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    methodOverride = require("method-override"),
+    MongoClient = require('mongodb').MongoClient,
+    port = process.env.PORT || 8080,
+    router = express.Router(),
+    app = express();
 
 //Routes
-var addingStudentRoutes     = require('./routes/addingStudents'),
+var addingStudentRoutes = require('./routes/addingStudents'),
     markingAttendanceRoutes = require('./routes/markingAttendance');
- 
-// make client connect to mongo service
-MongoClient.connect("mongodb://localhost:27017/AttendanceApp", (err, client) =>{
-    if (err){
-        return console.log("unable to connect to MongoDB server");
-    }
-    console.log("Database created!");
-    const db = client.db('AttendanceApp')
-    // db.collection('Attendance')
-    client.close();
-}); 
 
+// make client connect to mongo service
+mongoose.connect('mongodb://localhost:27017/AttendanceApp', {
+    useNewUrlParser: true
+});
+//on connected
+mongoose.connection.on('connected', () => console.log('connected to database :)'));
+//on error
+mongoose.connection.on('error', (err) => {
+    if (err) {
+        console.log('error is ' + err + config.SECRETS.database.url);
+    }
+});
 // mongoose.connect("mongodb://localhost:27017/attendance", { useUnifiedTopology: true });
 // MongoClient.connect({ useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res)=> {
-    res.json({ message: 'hooray! welcome to our api!' });   
+app.get('/', (req, res) => {
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
     console.log("Middleware has been established")
     next();
 })
 
 //routes
 // router.route('/admin/addStudents').get((req,res) => {addingStudentRoutes});
-app.use('/admin/addStudents' ,addingStudentRoutes);
-app.use('/teacher/attendance',markingAttendanceRoutes);
+app.use('/admin/addStudents', addingStudentRoutes);
+app.use('/teacher/attendance', markingAttendanceRoutes);
 
 app.post('/api/student')
 
-app.listen(port , () => {
+app.listen(port, () => {
     console.log("Server runs at 8080");
 })
