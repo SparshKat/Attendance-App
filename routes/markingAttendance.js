4432cc0c44e02dbbe46fc14df6c6b46ec530de10
Sparsh Katiyar <sparshkat99@gmail.com>
2020-07-18 23:44:59 +0530

    slight change in mark attendance route works with frontend

--- routes/markingAttendance.js
+++ routes/markingAttendance.js
@@ -10,46 +10,41 @@ var router = express.Router(),
 //Input of all the students present and mark students present based on if they are 
 //a part of the specified class
 router.put('/mark', (req, res) => {
-    request(`http://localhost:3000/student_retrieved.json`, (error, response, body) => {
-        if (error) {
-            console.log(error);
-            res.json({ message : "Error has occured in request"});
-        } else {
-            var retrieved = JSON.parse(body);
-            var ongoingClass = retrieved.classCode;
-            console.log(retrieved.students);
 
-            retrieved.students.forEach((stud, i) => {
-                // console.log(stud);
-                Student.find({ name: stud.name })
-                    .then(foundStudent => {
-                        if (foundStudent.length != 0) {
-                            // console.log(foundStudent[0]);
-                            foundStudent[0].attendanceID.forEach((el, i) => {
-                                if (el.subject === ongoingClass) {
-                                    // console.log(el);
-                                    var day = retrieved.day;
-                                    var week = retrieved.week;
-                                    var attendanceObj = {
-                                        day ,
-                                        week,
-                                        attended : 1
-                                    }
-                                    el.attendance.push(attendanceObj);
-                                    foundStudent[0].save();
-                                    console.log(foundStudent[0]);
-                                    // console.log("day -> " + day  + "week -> " + week);
-                                } else {
-                                    console.log("he hasn't opted for the course yet");
-                                }
-                            })
+    var retrieved = req.body;
+    var ongoingClass = retrieved.classCode;
+    console.log(retrieved.students);
+
+    retrieved.students.forEach((stud, i) => {
+        // console.log(stud);
+        Student.find({ name: stud.name })
+            .then(foundStudent => {
+                if (foundStudent.length != 0) {
+                    // console.log(foundStudent[0]);
+                    foundStudent[0].attendanceID.forEach((el, i) => {
+                        if (el.subject === ongoingClass) {
+                            // console.log(el);
+                            var day = retrieved.day;
+                            var week = retrieved.week;
+                            var attendanceObj = {
+                                day,
+                                week,
+                                attended: 1
+                            }
+                            el.attendance.push(attendanceObj);
+                            foundStudent[0].save();
+                            console.log(foundStudent[0]);
+                            // console.log("day -> " + day  + "week -> " + week);
+                        } else {
+                            console.log("he hasn't opted for the course yet");
                         }
                     })
-                    .catch(err => console.log("none found" + err));
-            });
-            res.redirect(`/admin/${retrieved.branch}/${ongoingClass}`)
-        }
-    })
+                }
+            })
+            .catch(err => console.log("none found" + err));
+    });
+    res.redirect(`/admin/${retrieved.branch}/${ongoingClass}`)
+
 
 })
 
